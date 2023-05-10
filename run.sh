29fe3bf1937667cb90491dac0f6279d587b1e9c7
Wei-Peng <30322673+pengwei-iie@users.noreply.github.com>
2023-05-10 13:40:32 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 nohup python finetune.py \
-    --base_model 'decapoda-research/llama-7b-hf' \
-    --data_path 'yahma/alpaca-cleaned' \
-    --batch_size '128' \
+    --base_model './path_to_llama-7b-hf' \
+    --data_path './alpaca_data_cleaned_archive.json' \
+    --batch_size '32' \
     --micro_batch_size '32' \
     --output_dir './lora-alpaca' > train_fine.out 2>&1 &
